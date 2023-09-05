d393c28a079a42a434857600f8bf335072d2e804
wisher-18 <johnnysonn10@gmail.com>
2023-09-05 18:27:37 +0530

    npm module

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,6 @@
+// const superhero = require("superhero-name-library");
+// var superHero = superhero.random();
+// console.log(`I am ${superHero}!`);
+import superheroes from "superheroes";
+var superHero =superheroes.random();
+console.log(`I am ${superHero}!`);
