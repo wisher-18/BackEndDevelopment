c04de75fef4ab887e6f6f452d070df5d1d33739c
wisher-18 <johnnysonn10@gmail.com>
2023-09-16 23:50:19 +0530

    16/09

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
@@ -11,12 +12,36 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function (req,res) {
-  var fName = req.body.firstName;
-  var lName = req.body.lastName;
-  var email = req.body.email;
-  console.log(fName, lName, email);
+  const fName = req.body.firstName;
+  const lName = req.body.lastName;
+  const email = req.body.email;
+
+  var data = {
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
+  const jsonData = JSON.stringify(data);
+  const url = "https://us21.api.mailchimp.com/3.0/lists/5ea1603db0"
+  https.request(url, options, function (response) {
+    
+  })
+
 });
 
 app.listen(3000, function(){
   console.log("Server is running on port: 3000");
 });
+
+
+// api key
+// 518a0297ba5e16ad51faf173371e1609-us21
+// List Id
+// 5ea1603db0
