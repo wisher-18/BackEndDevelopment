78bb52126e4153cb16a28b643a6596357712673e
wisher-18 <johnnysonn10@gmail.com>
2023-09-21 01:04:29 +0530

    21/09

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -16,24 +16,40 @@ app.post("/", function (req,res) {
   const lName = req.body.lastName;
   const email = req.body.email;
 
-  var data = {
+  const data = {
     members:[
       {
         email_address:email,
         status:"subscribed",
         merge_fields:{
-          FNAME: firstName,
-          LNAME: lastName
+          FNAME: fName,
+          LNAME: lName
         }
       }
     ]
   }
   const jsonData = JSON.stringify(data);
-  const url = "https://us21.api.mailchimp.com/3.0/lists/5ea1603db0"
-  https.request(url, options, function (response) {
-    
-  })
+  const url = "https://us21.api.mailchimp.com/3.0/lists/5ea1603db0";
 
+  const options = {
+    method:"POST",
+    auth:"wisher:20e9f4513efcde57912d9964f0821984-us21"
+  }
+
+  const request = https.request(url, options, function (response) {
+    if(response.statusCode === 200){
+      res.send("Successfully subscribed!");
+    }else {
+      res.send("Please try again!");
+    }
+    response.on("data",function (data) {
+      console.log(JSON.parse(data));
+
+    });
+  });
+  request.write(jsonData);
+  request.end();
+  res.sendFile(__dirname+"/success.html");
 });
 
 app.listen(3000, function(){
@@ -42,6 +58,6 @@ app.listen(3000, function(){
 
 
 // api key
-// 518a0297ba5e16ad51faf173371e1609-us21
+// 20e9f4513efcde57912d9964f0821984-us21
 // List Id
 // 5ea1603db0
